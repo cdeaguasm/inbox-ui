732d217e939d1e93f016a4c2185a1feb51b1b955
carlos de aguas manga <cdeaguasm@gmail.com>
2019-11-09 20:51:32 -0500

    Create inbox app

---
+++ Inbox/Inbox/Inbox/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Inbox.Models;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Inbox.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public List<Conversation> Messages { get; set; } = Conversations.All;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ Inbox/Inbox/Inbox/Views/InboxPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inbox.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inbox.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InboxPage : ContentPage
+    {
+        public InboxPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
